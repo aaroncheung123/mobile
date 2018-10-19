1edbafa251e263da3259d83d603a7e99cbd02bb1
Aaron Cheung <aaroncheung123@gmail.com>
2018-10-19 13:44:52 -0600

    Menu is now at the bottom

--- PogoPass/views/account/index.js
+++ PogoPass/views/account/index.js
@@ -52,8 +52,34 @@ export default class AccountNavigation extends React.Component {
           </ScrollView>
 
           {/*Bottom Menu*/}
-          <View>
-            <Text>Hello</Text>
+          <View style={STYLES.accountMenu.container}>
+            <View style={STYLES.accountMenu.menuContainer}>
+              <AccountMenuItem
+                onPress={() => this.updatePath('/notifications')}
+                active={path === '/notifications'}
+                icon="bell"
+              />
+              <AccountMenuItem
+                onPress={() => this.updatePath('/account-information')}
+                active={path === '/account-information'}
+                icon="user"
+              />
+              <AccountMenuItem
+                onPress={() => this.updatePath('/pass-manager')}
+                active={path === '/pass-manager'}
+                icon="ticket"
+              />
+              <AccountMenuItem
+                onPress={() => this.updatePath('/events')}
+                active={path === '/events'}
+                icon="calendar"
+              />
+              <AccountMenuItem
+                onPress={() => this.updatePath('/refer')}
+                active={path === '/refer'}
+                icon="dollar"
+              />
+            </View>
           </View>
 
         </View>
@@ -62,6 +88,22 @@ export default class AccountNavigation extends React.Component {
 
     )
   }
+}
+
+const AccountMenuItem = (props) => {
+  return (
+    <TouchableWithoutFeedback onPress={props.onPress}>
+      <View style={STYLES.accountMenu.menuItemContainer}>
+        <Icon
+          name={props.icon}
+          type='font-awesome'
+          color={props.active ? ELITE_WORKS_ORANGE : '#dddddd'}
+          size={30}
+        />
+        <Text style={{color: (props.active ? ELITE_WORKS_ORANGE : '#dddddd')}}>{props.title}</Text>
+      </View>
+    </TouchableWithoutFeedback>
+  )
 }
 
 const WorkSpaceSideBarRow = (props) => {
@@ -99,6 +141,30 @@ const STYLES = {
       right: 35,
       textAlign: 'center',
       color: '#dddddd'
+    }
+  },
+
+  accountMenu: {
+    container: {
+      flex: 1,
+      width: '100%',
+      backgroundColor: 'white',
+      maxHeight: 65,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    menuContainer: {
+      flexDirection: 'row',
+      flex: 1,
+      maxWidth: 300,
+      width: '100%'
+    },
+    menuItemContainer: {
+      width: '20%',
+      height: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'white'
     }
   },
   container: {
