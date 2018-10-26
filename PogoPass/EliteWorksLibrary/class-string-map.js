66fd85a2795861fea4acc244b4f7201e594421d9
Kyle Paulson <kyle.paulson92@gmail.com>
2018-10-26 11:20:55 -0600

    updated address

--- PogoPass/EliteWorksLibrary/class-string-map.js
+++ PogoPass/EliteWorksLibrary/class-string-map.js
@@ -8,7 +8,8 @@ const CLASS_STRING_MAP = () => {
         workorderproduct: EliteAPI.Models.STR.WorkOrderProduct,
         blog: EliteAPI.Models.CMS.Blog,
         post: EliteAPI.Models.CMS.Post,
-        shippingaddress: EliteAPI.Models.STR.ShippingAddress
+        shippingaddress: EliteAPI.Models.STR.ShippingAddress,
+        address: EliteAPI.Models.CRM.Address
     }
 }
 export default CLASS_STRING_MAP;
