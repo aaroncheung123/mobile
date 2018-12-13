335575624d08ee86a679d3e9736a59cf4dd47087
Kyle Paulson <kyle.paulson92@gmail.com>
2018-12-13 14:29:50 -0700

    pointed to stagin

--- EliteWorksLibrary/api/web-client.js
+++ EliteWorksLibrary/api/web-client.js
@@ -3,7 +3,7 @@ export default class WebClient {
 
 	static getFullUrl(url)
 	{
-		return `https://${GlobalUtil.webClientKey}.eliteworks.com${url}`;
+		return `http://${GlobalUtil.webClientKey}.staging.eliteworks.com${url}`;
 	}
 
 	static addApiKey(args)
